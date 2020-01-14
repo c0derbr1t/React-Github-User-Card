ba9ea879a705ae2afcd8a70fdb5823b7a9afec50
Lambda_School_Loaner_252 <c0der.br1t@gmail.com>
2020-01-14 16:26:43 -0600

    added components and basic functionality to app.js.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
+import PeopleList from './components/PeopleList';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { token } from './assets/auth.js';
+
+class App extends React.Component {
+  state = {
+    user: [],
+    userName: '',
+    people: []
+  };
+
+  componentDidMount() {
+    let options = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+
+    axios
+      .get('https://api.github.com/users/c0derbr1t', options)
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          user: [response.data],
+          userName: response.data.login
+        })
+      })
+      .catch(error => console.log(error));
+
+    axios
+      .get('https://api.github.com/users/c0derbr1t/followers', options)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          people: [res.data]
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleChanges = e => {
+    this.setState({
+      userName: e.target.value
+    });
+  }
+
+  fetchPeople = e => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(response => {
+        this.setState({
+          user: [response.data]
+        })
+      })
+      .catch(error => console.log(error));
+
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}/followers`)
+      .then(res => {
+        this.setState({
+          people: [res.data]
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello {this.state.userName}</h1>
+        <div>
+          <input
+            type='text'
+            value={this.state.userName}
+            onChange={this.handleChanges}
+          />
+          <button onClick={this.fetchPeople}>Go!</button>
+        </div>
+        <div>
+          <PeopleList user={this.state.user} people={this.state.people} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/PeopleList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import People from './People';
+
+const PeopleList = props => {
+    return(
+        <div>
+            This is where the people are
+        </div>
+    )
+}
+
+export default PeopleList
